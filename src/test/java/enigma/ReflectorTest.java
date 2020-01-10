3d52979ebf5dbbddc9fd4faa4fd7986fcdf5174c
gg266712 <gg266712@chichester.esirem-ad.ad.u-bourgogne.fr>
2020-01-10 15:34:42 +0100

    Test des classes ROTOR et REFLECTOR

--- src/test/java/enigma/ReflectorTest.java
+++ src/test/java/enigma/ReflectorTest.java
@@ -5,10 +5,14 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class ReflectorTest {
+	
+	Rotor rotor2 = Rotor.rotorFactory("B A C D E F G H I J K L M N O P Q R S T U V W X Y Z", "Q");
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testConvertForward_whenA_then_B(){
+		rotor2.setPosition(0);
+		int expected =1;		
+		assertEquals(expected,rotor2.convertBackward(0));
+					
 	}
-
 }
--- src/test/java/enigma/RotorTest.java
+++ src/test/java/enigma/RotorTest.java
@@ -5,10 +5,32 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class RotorTest {
-
+	
+	
+	
+	Rotor rotor = Rotor.rotorFactory("A B C D E F G H I J K L M N O P Q R S T U V W X Y Z", "Q");
+	Rotor rotor2 = Rotor.rotorFactory("B A C D E F G H I J K L M N O P Q R S T U V W X Y Z", "Q");
+	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testAdvance() {
+		int position = 1;
+		int expected =position +1;
+		rotor.setPosition(1);
+		rotor.advance();
+		assertEquals(expected,rotor.getPosition());
+		
 	}
+	@Test
+	public void testConvertBackward_whenA_then_B(){
+		rotor2.setPosition(0);
+		int expected =1;		
+		assertEquals(expected,rotor2.convertBackward(0));
+					
+	}
+	
+	
+		
+	
+	
 
 }
